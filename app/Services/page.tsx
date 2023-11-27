29c74ea9adf5cb7674f3f926209ed81cc73b922d
muhammadzee786 <ahtishamali1536@gmail.com>
2023-11-27 14:37:55 +0500

    feat: working on services

--- app/Services/page.tsx
+++ app/Services/page.tsx
@@ -1,22 +1,53 @@
 import React from 'react'
 import styles from './Services.module.css'
+import ReactIcon from '../assests/svg/reactIcon'
+
+const services = [
+     {
+          "icon": <ReactIcon />,
+          "title": "Web Design",
+          "description": "Most common methods for designing websites that work well on desktop.."
+     },
+     {
+          "icon": <ReactIcon />,
+          "title": "Mobile App",
+          "description": "Most common methods for designing websites that work well on desktop.."
+     },
+     {
+          "icon": <ReactIcon />,
+          "title": "UI/UX",
+          "description": "Most common methods for designing websites that work well on desktop.."
+     },
+     {
+          "icon": <ReactIcon />,
+          "title": "SEO Marketing",
+          "description": "Most common methods for designing websites that work well on desktop.."
+     },
+]
 
 function Services() {
      return (
           <div className={`${styles.container}`}>
                <div className={`${styles.serviceHead}`}>
                     <p className={`${styles.serviceTitle}`}>Services</p>
-                    <p className={`${styles.serviceSubHead}`}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                    <div className={`${styles.lineMf}`}/>
+                    <p className={`${styles.serviceSubHead}`}>What I do</p>
+                    {/* <div className={`${styles.lineMf}`}/> */}
                </div>
 
-               <div className={`${styles.serviceBox}`}>
-                    <div className={`${styles.serviceIco}`}>
-                         <span className={`${styles.icoCircle}`}>
-                              <i className=''></i>
-                         </span>
-                    </div>
+               <div className={`${styles.innerContainer}`}>
+                    {services.map((data: any, index: number) => {
+                         return (
+                              <div className={`${styles.listInner}`}>
+                                   <span className={`${styles.icon}`}>
+                                        {data.icon}
+                                   </span>
+                                   <h3 className={`${styles.title}`}>{data.title}</h3>
+                                   <p>{data.description}</p>
+                              </div>
+                         )
+                    })}
                </div>
+
           </div>
      )
 }
